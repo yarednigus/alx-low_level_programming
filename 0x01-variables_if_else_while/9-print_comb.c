3711d481232dc6c7b9100ecc73847d045b816ac2
yarednigus <nigussieyared344@gmail.com>
2023-06-16 13:56:57 -0700

    hot fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all 
+ * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -20,3 +20,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
