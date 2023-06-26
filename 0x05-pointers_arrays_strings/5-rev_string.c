d61a655d4abd7048e0ebf871e359bd56207eac22
yarednigus <nigussieyared344@gmail.com>
2023-06-26 13:44:10 -0700

    cone

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,17 @@
 #include "main.h"
+#include <string.h>
 
 /**
- *
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
+void rev_string(char *s) {
+   int i, j;
+   char temp;
+   for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+      temp = s[i];
+      s[i] = s[j];
+      s[j] = temp;
+   }
+}
