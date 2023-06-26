f129b7050538754b34cbe79ea74338536f1f35f1
yarednigus <nigussieyared344@gmail.com>
2023-06-26 13:57:46 -0700

    push

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,10 +10,10 @@ void rev_string(char *s)
 {
 	int i, j;
 	char temp;
-   	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
 	{
-      		temp = s[i];
-      		s[i] = s[j];
-     	 	s[j] = temp;
-   }
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
 }
