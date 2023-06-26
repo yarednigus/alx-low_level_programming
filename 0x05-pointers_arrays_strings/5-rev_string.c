088cb66e67e469f367433474bae910133b455c80
yarednigus <nigussieyared344@gmail.com>
2023-06-26 13:53:44 -0700

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,12 +6,14 @@
  * @s: Input string
  * Return: String in reverse
  */
-void rev_string(char *s) {
-   int i, j;
-   char temp;
-   for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
-      temp = s[i];
-      s[i] = s[j];
-      s[j] = temp;
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+   	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	{
+      		temp = s[i];
+      		s[i] = s[j];
+     	 	s[j] = temp;
    }
 }
