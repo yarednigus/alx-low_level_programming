e7337e1260691dec48acaf1c99972331c3c0d008
yarednigus <nigussieyared344@gmail.com>
2023-06-26 13:26:06 -0700

    bad

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,7 +7,7 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str++);
+		putchar(*str++);
 	}
-		_putchar('\n');
+		putchar('\n');
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_rev: print str in reverse
  * @s: para
  * Return: 0.
  */
-void print_rev(char *s)
-{
+void print_rev(char *s) {
+    int len = strlen(s);
 
+    for (int i = len - 1; i >= 0; i--) {
+        putchar(s[i]);
+    }
+
+    putchar('\n');
+}
