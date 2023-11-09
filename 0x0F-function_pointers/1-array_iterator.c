c3a5fc9268cd8c93b679b0a530a0426c08772875
yarednigus <yarednigus21@gmail.com>
2023-11-09 06:02:50 -0800

    project done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,24 +1,21 @@
 #include "function_pointers.h"
-	#include <stdio.h>
-	/**
-	 * array_iterator - prints each array elem on a newl
-	 * @array: array
-	 * @size: how many elem to print
-	 * @action: pointer to print in regular or hex
-	 * Return: void
-	 */
-	void array_iterator(int *array, size_t size, void (*action)(int))
+#include <stdio.h>
+/**
+ * array_iterator - write the array elements
+ * @array: is an array
+ * @size: magnitude
+ * @action: pointer to print in regular or hex
+ * Return: nothing (void)
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int count;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (count = 0; count < size; count++)
 	{
-		unsigned int i;
-
-
-		if (array == NULL || action == NULL)
-			return;
-
-
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+		action(array[count]);
 	}
-
+}
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,25 +1,22 @@
 #include "function_pointers.h"
-	/**
-	 * int_index - return index place if comparison = true, else -1
-	 * @array: array
-	 * @size: size of elements in array
-	 * @cmp: pointer to func of one of the 3 in main
-	 * Return: 0
-	 */
-	int int_index(int *array, int size, int (*cmp)(int))
-	{
-		int i;
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array: is an array
+ * @size: no of elements in array
+ * @cmp: a ptr to one of the 3 fx's in main
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int count;
 
-
-		if (array == NULL || size <= 0 || cmp == NULL)
-			return (-1);
-
-
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]))
-				return (i);
-		}
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	}
 
+	for (count = 0; count < size; count++)
+	{
+		if (cmp(array[count]))
+			return (count);
+	}
+	return (-1);
+}
