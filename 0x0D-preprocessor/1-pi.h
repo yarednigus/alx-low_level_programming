a3bbffcb83b9ef74bb28dc2cf62f5065381a2ba8
yarednigus <yarednigus21@gmail.com>
2023-11-18 10:51:54 -0800

    hot fix

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef PI_VALUE_H
+#define PI_VALUE_H
+
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-prints the name of the source file that it was compiled from
+ * return: 0(success)
+ * /
+ int main (void)
+ {
+ 	printf("Compiled from the file: %s\n", __FILE__);
+	return (0);
+ }
